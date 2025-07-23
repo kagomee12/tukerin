9c6ce471bda21b59aabafefdc1fb7579350bbbc9
kagomee12 <kalfitrah@gmail.com>
2025-07-24 00:02:17 +0700

    feat: add DTOs for Category, Product, Role, and User types

---
+++ type/product_dto.go
@@ -0,0 +1,11 @@
+package types
+
+type ProductsDTO struct {
+	Name        string  `json:"name" form:"name"`
+	Description string  `json:"description" form:"description"`
+	Price       float64 `json:"price" form:"price"`
+	CategoryId  int     `json:"category_id" form:"category_id"`
+	Category   CategoryDTO `json:"category" form:"category"`
+	UserId      int     `json:"user_id" form:"user_id"`
+	User        UserDTO `json:"user" form:"user"`
+}
