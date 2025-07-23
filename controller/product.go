e829986a5018f315ea7f9d13315d0a20215722fd
kagomee12 <kalfitrah@gmail.com>
2025-07-24 00:02:57 +0700

    feat: implement CRUD operations for Product controller

---
+++ controller/product.go
@@ -0,0 +1,171 @@
+package controller
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"tukerin/config"
+	"tukerin/models"
+	"tukerin/type"
+
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetProducts(c *gin.Context) {
+	var products []models.Product
+	var data []types.ProductsDTO
+	
+	_, exists := c.Get("user_id")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized access"})
+		return
+	}
+
+	pageStr := c.Query("page")
+	limitStr := c.Query("limit")
+
+	page, err := strconv.Atoi(pageStr)
+	if err != nil || page < 1 {
+		page = 1
+	}
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil || limit < 1 {
+		limit = 10
+	}
+
+	offset := (page - 1) * limit
+
+	var total int64
+
+	if err := config.DB.Model(&models.Product{}).Count(&total).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to count products"})
+		return
+	}
+
+	if err := config.DB.Limit(limit).Offset(offset).Preload("Category").Preload("User").Preload("User.Role").Find(&products).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch products"})
+		return
+	}
+
+	for _, product := range products {
+		data = append(data, types.ProductsDTO{
+			Name: 	  product.Name,
+			Description: product.Description,
+			Price:	  product.Price,
+			CategoryId: product.CategoryId,
+			UserId:	  product.UserId,
+			Category: types.CategoryDTO{
+				ID:   product.Category.ID,
+				Name: product.Category.Name,
+			},
+			User: types.UserDTO{
+				ID:    product.User.ID,
+				Name:  product.User.Name,
+				Email: product.User.Email,
+				RoleId: strconv.Itoa(product.User.RoleId),
+				Role: types.RoleDTO{
+					ID:   product.User.Role.ID,
+					Name: product.User.Role.Name,
+				},
+			},
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"data": data,
+		"limit": limit,
+		"page": page,
+		"total": total,
+		"totalPages": int(math.Ceil(float64(total) / float64(limit))),
+	})
+}
+
+func GetProductByID(c *gin.Context) {
+	var product models.Product
+	productID := c.Param("id")
+
+	userID, exists := c.Get("user_id")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized access"})
+		return
+	}
+
+	if err := config.DB.Where("id = ? AND user_id = ?", productID, userID).Preload("Category").Preload("User").First(&product).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": product})
+}
+
+func CreateProduct(c *gin.Context) {
+	var product models.Product
+
+	if err := c.ShouldBind(&product); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	userID, exists := c.Get("user_id")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized access"})
+		return
+	}
+
+	product.UserId = userID.(int)
+
+	if err := config.DB.Where("id = ?", product.CategoryId).First(&models.Category{}).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid category ID"})
+		return
+	}
+
+	if err := config.DB.Select("name", "description", "price", "category_id", "user_id").Create(&product).Error; err != nil {
+		fmt.Println("Error creating product:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create product"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"success": "Product created successfully"})
+}
+
+func UpdateProduct(c *gin.Context) {
+	var product models.Product
+	productID := c.Param("id")
+
+	if err := c.ShouldBind(&product); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	userID, exists := c.Get("user_id")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized access"})
+		return
+	}
+
+	if err := config.DB.Model(&product).Where("id = ? AND user_id = ?", productID, userID).Updates(product).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update product"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Product updated successfully"})
+}
+
+func DeleteProduct(c *gin.Context) {
+	productID := c.Param("id")
+
+	userID, exists := c.Get("user_id")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized access"})
+		return
+	}
+
+	if err := config.DB.Where("id = ? AND user_id = ?", productID, userID).Delete(&models.Product{}).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete product"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Product deleted successfully"})
+}
