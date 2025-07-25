ac3bcf1cd77f58ab668a678fe305b30d6d29fb59
kagomee12 <kalfitrah@gmail.com>
2025-07-25 15:57:36 +0700

    feat: add ImageDTO type and integrate with ProductsDTO for image handling

--- type/product_dto.go
+++ type/product_dto.go
@@ -1,11 +1,12 @@
 package types
 
 type ProductsDTO struct {
-	Name        string  `json:"name" form:"name"`
-	Description string  `json:"description" form:"description"`
-	Price       float64 `json:"price" form:"price"`
-	CategoryId  int     `json:"category_id" form:"category_id"`
-	Category   CategoryDTO `json:"category" form:"category"`
-	UserId      int     `json:"user_id" form:"user_id"`
-	User        UserDTO `json:"user" form:"user"`
+	Name        string      `json:"name" form:"name"`
+	Description string      `json:"description" form:"description"`
+	Price       float64     `json:"price" form:"price"`
+	CategoryId  int         `json:"category_id" form:"category_id"`
+	Category    CategoryDTO `json:"category" form:"category"`
+	UserId      int         `json:"user_id" form:"user_id"`
+	User        UserDTO     `json:"user" form:"user"`
+	Images      []ImageDTO    `json:"image" form:"image"`
 }
